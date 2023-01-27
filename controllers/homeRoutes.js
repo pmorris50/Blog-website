63f9c2e60a11bacf2bd589edacf7e1de302d27d1
pmorris50 <patrickmorris532@gmail.com>
2023-01-27 12:29:28 -0700

    saving

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth')
+const {Blog, User} = require('../models')
 // const {} = require('../models') //model classes go here
 
 
 
-router.get('/', (req, res) =>{
+
+  router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const blogData = await Blog.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['first_name', 'last_name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const blogs = blogData.map((blogmapped) => blogmapped.get({ plain: true }));
     console.log('/get homepage');
-    res.render('homepage');
-})
+    res.render('homepage', blogs);
+
+    })
+
 
 router.get('/profile', withAuth, async (req, res) => {
     try {
